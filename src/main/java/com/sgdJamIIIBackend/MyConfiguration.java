4437ad9ef28e9868b20c7101abe7620ac6ad9fdf
pedro876 <pedrocasas876@gmail.com>
2021-09-22 09:54:13 +0200

    Update MyConfiguration.java

--- src/main/java/com/sgdJamIIIBackend/MyConfiguration.java
+++ src/main/java/com/sgdJamIIIBackend/MyConfiguration.java
@@ -1,17 +1,9 @@
 package com.sgdJamIIIBackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 public class MyConfiguration {
@@ -21,7 +13,7 @@ public class MyConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
